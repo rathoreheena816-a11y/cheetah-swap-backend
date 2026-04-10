36ca1b4d48a74cbca3320537fa650fc7fb27e0e7
Heena <rathoreheena816@gmail.com>
2026-04-11 04:25:30 +0530

    fix port issue final

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send("LIVE 🚀");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
